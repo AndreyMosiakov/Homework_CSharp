5a4c5744026ff4638ebda6d5817716c208f56ed7
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-09 14:16:37 +0300

    add task10

---
+++ HW2_task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+    return number;
+}
+int SearchSecondDigit(int number)
+{
+    int digit12 = number / 10;// деление без остатка на 10, что бы убрать последнюю цифру
+    int result = digit12 % 10; // деление c остаком , где остатком как раз будет второе число
+    return result;
+}
+int randomNumber = GetNumberFromRange(100, 999);
+int secondNumber = SearchSecondDigit(randomNumber);
+Console.WriteLine($"Число {randomNumber} без первой и третьей цифры {secondNumber}"); 
+
