889fac91038b59b387202065c174d974ec464141
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-18 20:31:23 +0300

    add task25

---
+++ HW4_task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int GetExponentOfNumber (int userA , int userB )
+{
+    int res = userA;
+    for (int i = 1; i < userB; i++)
+    {
+        res = res*userA;
+    }
+    return res;
+}
+int userA = getUserValue("Введите число А");
+int userB = getUserValue("Введите число B");
+int res = GetExponentOfNumber(userA, userB);
+Console.WriteLine($"Число {userA} в {userB} степени равно {res}.");
