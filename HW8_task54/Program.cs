1644fd221682dd4f757fcf9249f0f63668df47c2
AndreyMosiakov <mosiakoff@yandex.ru>
2023-02-12 16:09:03 +0300

    add task54

---
+++ HW8_task54/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.Clear();
+
+int[,] Get2Darray(int rowlenght, int cowlenght, int start, int end)
+{
+    int[,] array = new int[rowlenght, cowlenght];
+
+    for (int i = 0; i < rowlenght; i++)
+    {
+
+        for (int j = 0; j < cowlenght; j++)
+        {
+            array[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return array;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t"); // знак табуляции , что бы всё выстроилось с правильными отступами
+        }
+        Console.WriteLine();
+    }
+
+}
+int[,] SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+
+int[,] array = Get2Darray(4, 4, 1, 10);
+print2DArray(array);
+Console.WriteLine("------------------------");
+int[,] sortedArray = SortArray(array);
+print2DArray(array);
