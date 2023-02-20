340b841491e3a7421940a123cb7831a11ef90439
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-02-20 15:26:27 +0300

    add task64

---
+++ HW9_task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+Console.Clear();
+
+int GetDataFromUser(string messege)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(messege);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int userStartNumber = GetDataFromUser("введите значение N");
+
+string rage = getStrRange(userStartNumber, 1);
+Console.WriteLine(rage);
+string getStrRange ( int start, int finish)
+{
+   finish =1; 
+   if(start==finish) 
+   {
+    return start +" ";
+   }
+   return start +","+ getStrRange(start-1, finish);
+
+}
